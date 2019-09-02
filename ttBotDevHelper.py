690dad99552a75dbc0aa92dbe59a3187c54b2c10
asvbkr <asv@bk.ru>
2019-09-02 18:51:38 +0300

    example 2

--- ttBotDevHelper.py
+++ ttBotDevHelper.py
@@ -3,6 +3,7 @@ import os
 
 from TamTamBot.TamTamBot import TamTamBot
 from TamTamBot.utils.lng import set_use_django
+from openapi_client import NewMessageBody
 
 
 class BotDevHelper(TamTamBot):
@@ -14,6 +15,32 @@ class BotDevHelper(TamTamBot):
     def description(self):
         return 'Этот бот помогает в разработке и управлении ботами.\n\n' \
                'This bot is an helper in the development and management of bots.'
+
+    def receive_text(self, update):
+        return bool(self.view_messages(update, [update.message.body.mid], update.link))
+
+    def cmd_handler_vmp(self, update):
+        res = None
+        if not update.this_cmd_response:  # Это прямой вызов команды, а не текстовый ответ на команду
+            if update.cmd_args:  # Если вместе с командой сразу переданы аргументы
+                list_id = []
+                parts = update.cmd_args.get('c_parts') or []
+                if parts:
+                    for line in parts:
+                        for part in line:
+                            list_id.append(str(part))
+                if list_id:
+                    res = self.view_messages(update, list_id, update.link)
+        return bool(res)
+
+    def view_messages(self, update, list_mid, link=None):
+        res = False
+        msgs = self.msg.get_messages(message_ids=list_mid)
+        if msgs:
+            for msg in msgs.messages:
+                r = self.msg.send_message(NewMessageBody(f'Сообщение {msg.body.mid}:\n`{msg}`'[:NewMessageBody.MAX_BODY_LENGTH], link=link), user_id=update.user_id)
+                res = res or r
+        return res
 
 
 if __name__ == '__main__':
